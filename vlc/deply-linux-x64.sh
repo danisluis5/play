28d205ca23c52ebaea18c828ff0543e41cd048a7
Alexey Kuznetsov <axet@me.com>
2012-12-25 09:06:45 +0400

    add deploy linux 64

---
+++ vlc/deply-linux-x64.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mvn gpg:sign-and-deploy-file \
+  -DuseAgent=true \
+  -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+  -DrepositoryId=sonatype-nexus-staging \
+  -DpomFile=libvlc-linux-x86_64.pom \
+  -Dclassifier=natives \
+  -Dpackaging=jar \
+  -Dfile=libvlc-linux-x86_64.jar
