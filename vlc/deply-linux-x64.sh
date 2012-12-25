13d0c14ad00aa6689f0de0a6989b9695bb8fec40
Alexey Kuznetsov <axet@me.com>
2012-12-25 09:26:55 +0400

    deploy empty source

--- vlc/deply-linux-x64.sh
+++ vlc/deply-linux-x64.sh
@@ -8,3 +8,21 @@ mvn gpg:sign-and-deploy-file \
   -Dclassifier=natives \
   -Dpackaging=jar \
   -Dfile=libvlc-linux-x86_64.jar
+
+mvn gpg:sign-and-deploy-file \
+  -DuseAgent=true \
+  -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+  -DrepositoryId=sonatype-nexus-staging \
+  -DpomFile=libvlc-linux-x86_64.pom \
+  -Dclassifier=sources \
+  -Dpackaging=jar \
+  -Dfile=empty.jar
+
+mvn gpg:sign-and-deploy-file \
+  -DuseAgent=true \
+  -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+  -DrepositoryId=sonatype-nexus-staging \
+  -DpomFile=libvlc-linux-x86_64.pom \
+  -Dclassifier=javadoc \
+  -Dpackaging=jar \
+  -Dfile=empty.jar
