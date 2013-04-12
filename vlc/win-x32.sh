0efd8df147d97ed0dba3e89b2e5d0ceb4c1df1d8
Alexey Kuznetsov <axet@me.com>
2013-04-12 09:22:05 -0400

    update win32 build

--- vlc/win-x32.sh
+++ vlc/win-x32.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-HOST=i686-w64-mingw32
+export HOST=i686-w64-mingw32
 
-sudo apt-get -t experimental install -y binutils-mingw-w64-i686 gcc-mingw-w64-i686 g++-mingw-w64-i686 mingw-w64-tools mingw-w64-i686-dev
+# minimum mingw-w64-3.0
+#
+# mingw-w64-i686-dev                    3.0~svn5496-1  
+# mingw-w64-tools                       3.0~svn5496-1  
+#
+sudo apt-get  -t experimental install -y binutils-mingw-w64-i686 gcc-mingw-w64-i686 g++-mingw-w64-i686 mingw-w64-tools mingw-w64-i686-dev || exit  1
 
-. ./win.sh || exit 1
+./win.sh || exit 1
 
 # pack:
 rm -f libvlc-windows-x86.jar || exit 1
@@ -12,8 +17,8 @@ rm -f libvlc-windows-x86.jar || exit 1
 rm -rf natives || exit 1
 mkdir -p natives || exit 1
 
-cp build/vlc-2.0.5/*.dll natives/ || exit 1
-cp -r build/vlc-2.0.5/plugins natives/ || exit 1
+cp build/vlc-2.0.6/*.dll natives/ || exit 1
+cp -r build/vlc-2.0.6/plugins natives/ || exit 1
 
 (cd natives && jar cf ../libvlc-windows-x86.jar *) || exit 1
 
--- vlc/win.sh
+++ vlc/win.sh
@@ -2,13 +2,13 @@
 
 (if ! [ -e ../../vlc/contrib/win32 ]; then mkdir -p ../../vlc/contrib/win32 && cd ../../vlc/contrib/win32 && ../bootstrap --host=$HOST; make prebuilt; fi) || exit 1
 
-rm -f ../../vlc/contrib/$HOST/include/strmif.h || exit 1
-rm -f ../../vlc/contrib/$HOST/include/uuids.h || exit 1
-rm -f ../../vlc/contrib/$HOST/include/dxva2api.h || exit 1
+#rm -f ../../vlc/contrib/$HOST/include/strmif.h || exit 1
+#rm -f ../../vlc/contrib/$HOST/include/uuids.h || exit 1
+#rm -f ../../vlc/contrib/$HOST/include/dxva2api.h || exit 1
 
 (if ! [ -e "../../vlc/configure" ]; then cd ../../vlc && autoreconf -fi && ./bootstrap; fi) || exit 1
 
-(if [ "../../vlc/configure" -nt "./build/Makefile" ]; then mkdir -p build && cd ./build/ && ../../../vlc/extras/package/win32/configure.sh --host=$HOST --with-contrib=`pwd`/../../../vlc/contrib/$HOST --enable-shared --disable-live555; fi) || exit 1
+(if [ "../../vlc/configure" -nt "./build/Makefile" ]; then mkdir -p build && cd ./build/ && ../../../vlc/extras/package/win32/configure.sh --host=$HOST --with-contrib=$PWD/../../../vlc/contrib/$HOST --enable-shared --disable-live555; fi) || exit 1
 
 (cd build && make)  || exit 1
 
